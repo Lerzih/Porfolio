3902c33ede3e2cf6d1b6fa6ec6e82089a462152b
Lerzih <lerzihbarriales@gmail.com>
2023-08-21 10:57:03 +0100

    [:sparkles:] Add Navigation component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,21 @@
+import "../css/Navigation.css";
+
+function Navigation() {
+  return (
+    <>
+      <div className="navigation">
+        <div className="navigation-container">
+          <div className="navigation-links">
+            <div>About Me</div>
+            <div>Skills</div>
+            <div>Experience</div>
+            <div>Projects</div>
+            <div>Contact</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navigation;
